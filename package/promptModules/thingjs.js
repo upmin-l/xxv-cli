0704e3eb30bc65fcda8e97c1921c1906b2b68357
upmin-l <904412200@qq.ocm>
2021-12-15 16:19:41 +0800

    fix:注入 thingjs or uearthjs

---
+++ package/promptModules/thingjs.js
@@ -0,0 +1,8 @@
+module.exports = cli => {
+    cli.injectFeature({
+        name: 'thingjs',
+        value: 'thing',
+        description: '面向物联网的3D可视化开发平台',
+        link: 'https://www.thingjs.com/guide/?m=main'
+    })
+}
---
+++ package/promptModules/uearth.js
@@ -0,0 +1,8 @@
+module.exports = cli => {
+    cli.injectFeature({
+        name: 'uearth',
+        value: 'uearth',
+        description: '面向物联网的3D可视化开发平台',
+        link: 'https://www.thingjs.com/guide/?m=main'
+    })
+}

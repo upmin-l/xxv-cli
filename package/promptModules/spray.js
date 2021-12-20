753b6d16e51d158e68cd24fc37012f6679cad256
upmin-l <904412200@qq.ocm>
2021-12-20 17:13:20 +0800

    feat:手动预选项注入 onPromptComplete

--- package/promptModules/spray.js
+++ package/promptModules/spray.js
@@ -5,4 +5,9 @@ module.exports = cli => {
         description: '一个构建自适应布局可视化场景的组件包',
         link: 'https://www.yuque.com/khth0u/ngd5zk'
     })
+    cli.onPromptComplete((answers, options) => {
+        if (answers.features.includes('spray')) {
+            options.plugins["spray"] = true
+        }
+    })
 }
--- package/promptModules/uearth.js
+++ package/promptModules/uearth.js
@@ -5,4 +5,9 @@ module.exports = cli => {
         description: '面向物联网的3D可视化开发平台',
         link: 'https://www.thingjs.com/guide/?m=main'
     })
+    cli.onPromptComplete((answers, options) => {
+        if (answers.features.includes('uearth')) {
+            options.plugins["uearth"] = {}
+        }
+    })
 }

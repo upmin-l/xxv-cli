788355d919286986c08314c9243254b372b7c1be
melan <lanshikeng@uinnova.com>
2022-01-13 17:39:28 +0800

    fix: template apply 回调

--- package/plugMode/cli.js
+++ package/plugMode/cli.js
@@ -1,6 +1,8 @@
 module.exports = (api, options) => {
     console.log('options', options);
-    api.render('./template', {})
+    api.render('./template', {
+        hasDefault:true
+    })
     // 如果默认创建的
     if (options.hasDefault) {
 

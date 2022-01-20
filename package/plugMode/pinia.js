abdb4cb9a0f6b3ed589e967910969cdd8fc15bb4
melan <lanshikeng@uinnova.com>
2022-01-20 16:49:36 +0800

    feat: 添加 项目必须文件包

---
+++ package/plugMode/pinia.js
@@ -0,0 +1,3 @@
+module.exports = (api,options)=>{
+    console.log(api);
+}
---
+++ package/plugMode/template/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+const { resolve } = require('path')
+
+
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+
+    return defineConfig({
+        plugins: [
+            vue(),
+        ],
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src'),
+            },
+        },
+        optimizeDeps: {
+            include: ['vue', 'vuex', 'vue-router'],
+        },
+        build: {
+            terserOptions: {
+                compress: {
+                    drop_console: true,
+                    pure_funcs: ['console.log'],
+                },
+            },
+        },
+    })
+}
